43157ec6b3030abc3db7de72381923c8e90cc85b
Samy <xsamyrslx@gmail.com>
2025-12-01 12:09:34 +0100

    controller

--- src/main/java/org/example/todoapp/HelloController.java
+++ src/main/java/org/example/todoapp/HelloController.java
@@ -1,12 +1,20 @@
 package org.example.todoapp;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class HelloController {
     @GetMapping("/")
     public String index() {
         return "Hello World!";
+    }
+
+    @GetMapping("/v2")
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("Hello WORLD!");
     }
 }
